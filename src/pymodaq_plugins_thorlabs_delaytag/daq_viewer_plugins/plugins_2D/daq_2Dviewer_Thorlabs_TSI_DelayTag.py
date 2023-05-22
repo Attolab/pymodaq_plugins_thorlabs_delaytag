80ee0ede20c6a8e6496ee508336572a093ec0316
rgeneaux <romain.geneaux@cea.fr>
2023-05-22 13:35:56 +0200

    Using number of samples rather than measurement time

--- src/pymodaq_plugins_thorlabs_delaytag/daq_viewer_plugins/plugins_2D/daq_2Dviewer_Thorlabs_TSI_DelayTag.py
+++ src/pymodaq_plugins_thorlabs_delaytag/daq_viewer_plugins/plugins_2D/daq_2Dviewer_Thorlabs_TSI_DelayTag.py
@@ -20,17 +20,19 @@ class DAQ_2DViewer_Thorlabs_TSI_DelayTag(DAQ_2DViewer_Thorlabs_TSI):
 
     params = DAQ_2DViewer_Thorlabs_TSI.params + [
         {'title': 'Delay Tagging', 'name': 'delay_opts', 'type': 'group', 'children':
-            [{'title': 'Acquisition Time (ms)', 'name': 'acq_time', 'type': 'int', 'value': 1},
-             {'title': 'Enable tagging', 'name': 'tag_on', 'type': 'bool', 'value': False}]
+            [{'title': 'Number of samples', 'name': 'acq_number', 'type': 'int', 'value': 10},
+             {'title': 'Enable tagging', 'name': 'tag_on', 'type': 'bool', 'value': False},
+             {'title': 'Acquis. time (ms)', 'name': 'acq_time', 'type': 'float', 'value': 0., 'readonly': True}]
          }]
 
-    acq_start_time = None
+    acq_counter = 0
+    acq_time = 0.
     data_tag = []
 
     def grab_data(self, Naverage=1, **kwargs):
         if self.settings.child('delay_opts','tag_on').value():
-            self.acq_start_time = perf_counter()  # Save time at which acquisition start
-
+            self.acq_counter = 0
+            self.acq_time = perf_counter() # Save time at which acquisition start
         super().grab_data(Naverage=1, **kwargs)
 
     def emit_data(self):
@@ -44,22 +46,25 @@ class DAQ_2DViewer_Thorlabs_TSI_DelayTag(DAQ_2DViewer_Thorlabs_TSI):
                 if frame is not None:
                     self.data_tag.append(np.sum(frame, axis=0))
 
-                # We stack fringe images till we reach "acq_time"
-                emit_time = perf_counter()
-                # Acq_time is reached
-                if emit_time - self.acq_start_time > self.settings.child('delay_opts', 'acq_time').value()/1000:
+                # We stack fringe images till we reach the sample number
+                self.acq_counter += 1
+
+                # Acq number is reached
+                if self.acq_counter >= self.settings.child('delay_opts', 'acq_number').value():
                     # Emit data
                     self.data_grabed_signal.emit([DataFromPlugins(name='Thorlabs Camera',
                                                                   data=[np.asarray(self.data_tag)],
                                                                   dim='Data2D',
                                                                   labels=[f'Delay tag'])])
+                    self.settings.child('delay_opts', 'acq_time').setValue(round((perf_counter()-self.acq_time)*1000,1))
+
                     # Empty data array
                     self.data_tag = []
                     # Reset counter
-                    self.acq_start_time = perf_counter()
+                    self.acq_counter = 0
+                    self.acq_time = perf_counter()
                 else:
                     self.callback_signal.emit()  # Wait for next acquisition
-
 
                     # To make sure that timed events are executed in continuous grab mode
                 QtWidgets.QApplication.processEvents()
